85dde56badf27eb374a26c9b1431e89ee013e445
Teguh Arief <114749743+teguharifudin@users.noreply.github.com>
2024-08-01 01:52:12 +0700

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { UpdateEmployeeComponent } from './update-employee/update-employee.compo
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 
 const routes: Routes = [
-  {path: 'employees', component: EmployeeListComponent},
-  {path: 'create-employee', component: CreateEmployeeComponent},
+  {path: 'employees', title: 'Dashboard', component: EmployeeListComponent},
+  {path: 'create-employee', title: 'Add new', component: CreateEmployeeComponent},
   {path: '', redirectTo: 'employees', pathMatch: 'full'},
   {path: 'update-employee/:id', component: UpdateEmployeeComponent},
   {path: 'employee-details/:id', component: EmployeeDetailsComponent}
